55af662da4c21f38aa0e49b02e925bd50e040ba1
zhangguowei-users <zhangguowei158@qq.com>
2020-05-09 10:02:30 +0800

    dsadsadsa

---
+++ js/config.js
@@ -0,0 +1,4 @@
+var config = {
+    url:"localhost:",
+    port:"5010"
+}
---
+++ js/login.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".login").click(function(){
+      var name = $("#name").val();
+      var password = $("#password").val();
+      $.ajax({
+          url:config.url + config.port + "/auth/Login/LoginCheck",
+          type: 'POST',
+          async: false,
+          xhrFields:{withCredentials:true},
+          data:{name:name,password:password},
+          success:function(data){
+              if(data.code == 0){
+                   location.href = "./index.html";
+              }else{
+                  alert("登陆失败");
+              }
+          }
+      })
+    });
+});
